16e79c873be3929ffafd80e52a6ec5622bc7c0aa
ibat10clw <tyjkhskjs957@gmail.com>
2022-03-26 07:58:10 +0800

    Use fast doubling in kernel space
    Original the fast doubling methot i implement in userspace, now I
    porting it to fibdrv and check the result is correct.

--- fibdrv.c
+++ fibdrv.c
@@ -52,28 +52,111 @@ static void add(char *a, char *b, char *out)
     }
     out[len_a] = 0;
 }
-static long my_fibo(int k, char *buf)
+static void sub(char *a, char *b, char *out)
 {
-    char f3[SIZE];
-    char f2[SIZE] = "1";
-    char f1[SIZE] = "0";
-    for (int i = 2; i <= k; ++i) {
-        add(f2, f1, f3);
-        strncpy(f1, f2, strlen(f2) + 1);
-        strncpy(f2, f3, strlen(f3) + 1);
+    int len_a = strlen(a);
+    int len_b = strlen(b);
+    int i = 0;
+    int borrow = 0;
+    int idx = len_a - len_b;
+    for (i = len_b - 1; i >= 0; i--) {
+        int diff = (a[i + idx] - '0') - (b[i] - '0') - borrow;
+        if (diff < 0) {
+            diff += 10;
+            borrow = 1;
+        } else
+            borrow = 0;
+        out[i + idx] = '0' + diff;
     }
+    for (i = idx - 1; i >= 0; i--) {
+        int diff = (a[i] - '0') - borrow;
+        if (diff < 0) {
+            diff += 10;
+            borrow = 1;
+        } else
+            borrow = 0;
+        out[i] = '0' + diff;
+    }
+    out[len_a] = 0;
+    i = 0;
+    while (out[i] == '0')
+        i++;
+    if (i != 0) {
+        for (int j = 0; j < len_a; ++j)
+            out[j] = out[j + i];
+    }
+}
+static void mul(char *a, char *b, char *out)
+{
+    int len_a = strlen(a);
+    int len_b = strlen(b);
+    int N = len_a + len_b;
+    int i;
+    memset(out, '0', N);
+    out[N] = 0;
+    for (i = len_b - 1; i >= 0; i--) {
+        for (int j = len_a - 1; j >= 0; j--) {
+            int digit1 = a[j] - '0';
+            int digit2 = b[i] - '0';
+            int pos = 1 + i + j;
+            int carry = out[pos] - '0';
+            int multiplication = digit1 * digit2 + carry;
+            out[pos] = (multiplication % 10) + '0';
+            out[pos - 1] += (multiplication / 10);
+        }
+    }
+
+    i = 0;
+    while (out[i] == '0')
+        i++;
+    if (i != 0) {
+        for (int j = 0; j < N; ++j)
+            out[j] = out[j + i];
+    }
+}
+static long fast_fib(int k, char *buf)
+{
     if (k == 0) {
-        copy_to_user(buf, f1, strlen(f1) + 1);
-        return 0;
+        copy_to_user(buf, "0", 2);
+        return k;
     }
+
     if (k == 1) {
-        copy_to_user(buf, f2, strlen(f2) + 1);
-        return 1;
+        copy_to_user(buf, "1", 2);
+        return k;
     }
-    copy_to_user(buf, f3, strlen(f3) + 1);
+    int stack[100];
+    int top = 0;
+    while (k > 1) {
+        stack[top++] = k;
+        k /= 2;
+    }
+    char f1[SIZE] = "1";
+    char f2[SIZE] = "1";
+    char tmp1[SIZE];
+    char tmp2[SIZE];
+    char res1[SIZE];
+    while (top--) {
+        int n = stack[top];
+        // f1 = f1 * (2 * f2 - f1)
+        mul("2", f2, tmp1);
+        sub(tmp1, f1, tmp2);
+        mul(tmp2, f1, res1);
+        // f2 = f1 ** 2 + f2 ** 2
+        mul(f1, f1, tmp1);
+        mul(f2, f2, tmp2);
+        add(tmp2, tmp1, f2);
+        if (n % 2 == 1) {
+            add(f2, res1, tmp1);
+            strncpy(f1, f2, strlen(f2) + 1);
+            strncpy(f2, tmp1, strlen(tmp1) + 1);
+        } else {
+            strncpy(f1, res1, strlen(res1) + 1);
+        }
+    }
+    copy_to_user(buf, f1, strlen(f1) + 1);
     return k;
 }
-
 static int fib_open(struct inode *inode, struct file *file)
 {
     if (!mutex_trylock(&fib_mutex)) {
@@ -95,7 +178,7 @@ static ssize_t fib_read(struct file *file,
                         size_t size,
                         loff_t *offset)
 {
-    return (ssize_t) my_fibo(*offset, buf);
+    return (ssize_t) fast_fib(*offset, buf);
     // return (ssize_t) fib_sequence(*offset);
 }
 
